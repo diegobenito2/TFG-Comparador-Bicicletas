404049b7f39099768f41a64c91eaf020c0d2ad1b
Diego <diegobenitomartinez02@gmail.com>
2025-04-15 21:22:53 +0200

    Commit 1.2

--- README.md
+++ README.md
@@ -4,23 +4,20 @@
 
 Este proyecto tiene como objetivo crear un comparador de bicicletas que permita a los usuarios filtrar y comparar diferentes modelos basados en varios criterios, como el tipo de cambio (mecánico o electrónico), el tipo de freno (disco o mecánico), el material del cuadro (carbono o aluminio), y el uso de la bicicleta (Carretera, Montaña, Gravel). La información será obtenida a través de **web scraping** de diferentes marcas y sitios web oficiales, lo que permitirá que el comparador esté actualizado con las bicicletas más recientes del mercado.
 
-El proyecto estará basado en un **back-end de Spring Boot** utilizando **Thymeleaf** como motor de plantillas para el front-end. La interfaz de usuario utilizará **Bootstrap** para asegurar una presentación moderna y responsiva.
+El proyecto estará basado en un **back-end de Spring Boot** utilizando **Thymeleaf** como motor de plantillas para el front-end. La interfaz de usuario utilizará **Tailwind CSS** para asegurar una presentación moderna y responsiva.
 
 ## Funcionalidades
 
 - **Filtrado de bicicletas**: Los usuarios podrán filtrar bicicletas por uso (Carretera, Montaña, Gravel), tipo de freno, tipo de cambio y material.
-- **Comparación de bicicletas**: Los usuarios podrán comparar bicicletas de diferentes marcas en función de las especificaciones seleccionadas.
-- **Obtención de datos**: Utilizando **web scraping**, se extraerán enlaces de colecciones de modelos de diferentes marcas (o una marca si el tiempo es limitado) y se almacenarán en una base de datos para evitar bloqueos por parte de los sitios web.
+- **Comparación de bicicletas**: Los usuarios podrán comparar bicicletas de diferentes marcas y modelos.
 - **Especificaciones detalladas**: Cada bicicleta mostrará sus especificaciones principales y tendrá un enlace que llevará al usuario a la página original para obtener más detalles o ver variantes de componentes y precios.
-- **Interfaz intuitiva**: Al ingresar a la página, los usuarios verán todas las bicicletas ordenadas por precio de mayor a menor, con un botón para ver más detalles y otro para comparar.
+- **Interfaz intuitiva**: Al ingresar a la página, los usuarios verán todas las bicicletas ordenadas por precio de mayor a menor, podiendo hacer clic en la imagen de la bicicleta para obtener más detalles y un botón de **"Comparar"** para comparar.
 
 ## Tecnologías
 
 - **Back-End**: Spring Boot
 - **Front-End**: Thymeleaf, Tailwind CSS
-- **Web Scraping**: Uso de herramientas específicas de web scraping para obtener los datos de las bicicletas de diferentes webs de marcas.
-- **Base de Datos**: Almacenamiento de los modelos de bicicletas en una base de datos para acceder rápidamente a los datos sin realizar scraping cada vez que se acceda al sitio.
-- **Integración con ChatGPT**: Posibilidad de usar inteligencia artificial para enriquecer los datos de las bicicletas o realizar consultas avanzadas.
+- **Base de Datos**: Almacenamiento de los modelos de bicicletas en una base de datos para acceder rápidamente a los datos.
 
 ## Estructura
 
@@ -39,8 +36,7 @@ Cada bicicleta tendrá los siguientes elementos:
 - Nombre del modelo.
 - Precio.
 - Especificaciones principales (freno, cambio, material).
-- Botón de "Ver" para obtener más detalles.
-- Botón de "Comparar" para añadir la bicicleta al comparador.
+- Botón de **"Comparar"** para añadir la bicicleta al comparador.
 
 ### 2. Página de Comparación
 
@@ -50,16 +46,18 @@ En la página de comparación, los usuarios podrán comparar las bicicletas que 
 
 Cada bicicleta tendrá un enlace debajo de las especificaciones con el texto "Más info", que llevará a los usuarios a la página oficial del modelo. En esa página, los usuarios podrán ver más detalles, como variaciones de componentes y precios diferentes según las configuraciones seleccionadas.
 
-## Web Scraping
+## Objetivo
+
+El objetivo de este proyecto es ayudar a los usuarios a tomar decisiones informadas al comparar diferentes bicicletas de manera sencilla y rápida, ahorrándoles el tiempo que perderían buscando en múltiples sitios web. Este comparador tiene como inspiración el comparador de bicicletas de Orbea, pero con la idea de expandirlo a una mayor variedad de marcas y modelos.
+
+## Funcionalidades futuras
+
+### Web Scraping
 
 El scraping se realizará utilizando herramientas específicas para acceder a las páginas de modelos de bicicletas de diferentes marcas (por ejemplo, Berria y otras marcas). Los datos de cada bicicleta se extraerán y almacenarán en una base de datos local, lo que evitará la necesidad de hacer scraping en cada solicitud de usuario.
 
-### Flujo de Web Scraping:
+#### Flujo de Web Scraping:
 
 1. **Extracción de enlaces**: Se accederá a páginas específicas de colecciones de bicicletas por tipo de uso y se extraerán los enlaces a los modelos.
 2. **Obtención de información de los modelos**: Se extraerán las especificaciones de cada bicicleta (precio, componentes, tipo de freno, etc.).
 3. **Almacenamiento en base de datos**: Los datos extraídos se almacenarán en una base de datos para que estén disponibles rápidamente sin necesidad de volver a hacer scraping.
-
-## Objetivo
-
-El objetivo de este proyecto es ayudar a los usuarios a tomar decisiones informadas al comparar diferentes bicicletas de manera sencilla y rápida, ahorrándoles el tiempo que perderían buscando en múltiples sitios web. Este comparador tiene como inspiración el comparador de bicicletas de Orbea, pero con la idea de expandirlo a una mayor variedad de marcas y modelos.
--- src/main/java/com/example/comparador/App.java
+++ src/main/java/com/example/comparador/App.java
@@ -22,7 +22,7 @@ public class App {
         var bicicletaRepository = context.getBean(BicicletaRepository.class);
         var bicicletaComponenteRepository = context.getBean(BicicletaComponenteRepository.class);
 
-        List<Componente> componentes = List.of(
+        List<Componente> componentesBerria = List.of(
                 new Componente("Belador 6 BR DISC, Racing Geometry BGC ASE, Full carbon UHM3X, Triple monocoque, T47 Bottom Bracket, Full internal cable system ICS2, Headset semi integrado OH2S, Flat Mount System brake, neumático máximo 700x34c, Eje pasante 12x142mm, peso 720gr.", TipoComponente.Cuadro),
                 new Componente("Belador Blade 6 BR horquilla de carbono completa, sistema de cableado interno ICS2, compatible con tubo de dirección cónico 1-1/8\" - 1-1/2\", sistema de freno Flat Mount, neumático máximo 700x34c, Eje pasante 12x100mm, peso 350g.", TipoComponente.Horquilla),
                 new Componente("Avanforce Airone, Manillar de carretera integral de carbono, sistema de cableado interno completo ICS2, tamaños 380x90 - 400x100 - 420x110 - 420x120, peso: 320gr.", TipoComponente.Manillar),
@@ -39,23 +39,41 @@ public class App {
                 new Componente("SRAM RED AXS E1, sistema de freno hidráulico Shift-Brake.", TipoComponente.Frenos),
                 new Componente("Ceramic Speed SRAM DUB T47 Bottom Bracket.", TipoComponente.Pedalier)
         );
+        List<Componente> componentesCanyon = List.of(
+                new Componente("Canyon Ultimate CFR - Cuadro de nivel profesional, carbono CFR, eje 12x142 mm, espacio para cubiertas 33MM. Alta rigidez y peso ligero.", TipoComponente.Cuadro),
+                new Componente("Canyon FK0108 CFR - Horquilla de carbono superligera, eje 12x100 mm, cubiertas hasta 33MM. Precisión y rigidez optimizadas.", TipoComponente.Horquilla),
+                new Componente("Canyon CP0018 Aerocockpit Diseño aerodinámico con paredes de carbono reforzadas para mayor rigidez y mayor durabilidad. Material: Carbono (CF) Peso: 195 g", TipoComponente.Manillar),
+                new Componente("Canyon SP0064 - Tija de sillín aerodinámica de carbono con retroceso de 20 mm y peso de 70 g.", TipoComponente.Tija_Sillin),
+                new Componente("Selle Italia SLR Boost Tekno Superflow - Máximo peso de ciclista: 100 kg.", TipoComponente.Sillin),
+                new Componente("DT Swiss ARC 1100 Dicut db - Llanta de carbono.", TipoComponente.Ruedas),
+                new Componente("Schwalbe Pro One TT, 28mm - Rueda 28\", peso 215 g.", TipoComponente.Cubiertas),
+                new Componente("Shimano Dura-Ace Di2 R9270, 12-speed - 2 pistones, 332 g.", TipoComponente.Manetas_Cambio),
+                new Componente("Shimano Dura-Ace Di2 RD-R9250, 12-speed - Caja larga, 217 g.", TipoComponente.Cambio_Trasero),
+                new Componente("Shimano Dura-Ace Di2 FD-R9250 - 96 g.", TipoComponente.Desviador),
+                new Componente("Shimano Dura-Ace Di2 R9200 Powermeter - 2 platos.", TipoComponente.Bielas),
+                new Componente("Shimano CN-M9100 12s", TipoComponente.Cadena),
+                new Componente("Shimano Dura-Ace CS-R9200 - 12 piñones, relación 11-30.", TipoComponente.Cassette),
+                new Componente("Shimano Dura-Ace RT-CL900", TipoComponente.Frenos),
+                new Componente("Shimano Pressfit BB92 - PF 86,5, 55 g.", TipoComponente.Pedalier)
+        );
         List<Bicicleta> bicicletas = List.of(
                 new Bicicleta("Oiz M10 TR", new BigDecimal("5499"), TipoBicicleta.MONTAÑA, MaterialCuadro.ALUMINIO, TipoCambio.MECANICO, new BigDecimal("12"), "https://www.orbea.com/img/products/product/over/large/S241TTCC-KT-SIDE-OIZ_MPRO_over.jpg", "https://www.orbea.com/es-es/bicicletas/montana/oiz/cat/oiz-m10", "Orbea")
-                , new Bicicleta("Scalpel Carbon 2", new BigDecimal("5299"), TipoBicicleta.MONTAÑA, MaterialCuadro.CARBONO, TipoCambio.MECANICO, new BigDecimal("12"), "https://embed.widencdn.net/img/dorelrl/rmhoza7v2v/1100px@1x/C23_C24301M_Scalpel_Crb_2_SNB_PD.webp?color=F6F6F6&q=99", "https://www.cannondale.com/es-es/bikes/mountain/cross-country/scalpel/scalpel-carbon-2", "Cannondale"),
+                ,
                 new Bicicleta("Belador BR RED AXS PWM", new BigDecimal("10990"), TipoBicicleta.CARRETERA, MaterialCuadro.CARBONO, TipoCambio.ELECTRONICO, new BigDecimal("6.5"), "https://berriabikes.com/cdn/shop/files/download_14585b77-7ada-40b8-b0a9-e2c7109dda67.jpg?v=1721117600&width=1100", "https://berriabikes.com/products/belador-br-red-axs-pwm?variant=42521808896099", "Berria")
                 , new Bicicleta("Mako BR XX-SL", new BigDecimal("11990"), TipoBicicleta.MONTAÑA, MaterialCuadro.CARBONO, TipoCambio.ELECTRONICO, new BigDecimal("8"), "https://berriabikes.com/cdn/shop/files/MAKO_BR_XX-SL_FA_C1_1FRONTAL_NAKEDCARBON-WHITEPEARL.jpg?v=1721125110&width=990", "https://berriabikes.com/products/mako-br-xx-sl", "Berria")
                 , new Bicicleta("ALLROAD HPR ELITE APEX", new BigDecimal("1499"), TipoBicicleta.GRAVEL, MaterialCuadro.ALUMINIO, TipoCambio.MECANICO, new BigDecimal("8.33"), "https://berriabikes.com/cdn/shop/files/ALLROAD_HPR_ELITE_APEX_1FRONTAL_bd39722c-0196-4ba1-a50c-9cb3c20bcc1b.jpg?v=1721255333&width=823", "https://berriabikes.com/products/allroad-hpr-elite-apex", "Berria")
+                , new Bicicleta("Ultimate CFR Di2 Aero", new BigDecimal("8629"), TipoBicicleta.CARRETERA, MaterialCuadro.CARBONO, TipoCambio.ELECTRONICO, new BigDecimal("6.64"), "https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dwa0285dac/images/full/full_2023_/2023/full_2023_3564_ultimate-cfr-di2_P04_P5.jpg?sw=1300&sfrm=png&q=90&bgcolor=F2F2F2", "https://www.canyon.com/es-es/bicicletas-de-carretera/bicicletas-carreras/ultimate/cfr/ultimate-cfr-di2-aero/3564.html?dwvar_3564_pv_rahmenfarbe=R102_P04", "Canyon")
         );
         bicicletaRepository.saveAll(bicicletas);
-        componenteRepository.saveAll(componentes);
+        componenteRepository.saveAll(componentesBerria);
+        componenteRepository.saveAll(componentesCanyon);
         List<BicicletaComponente> bicicletaComponentes = new ArrayList<>();
-        for (Componente componente : componentes) {
-            bicicletaComponentes.add(new BicicletaComponente(bicicletas.get(2), componente));
+        for (Componente componente : componentesBerria) {
+            bicicletaComponentes.add(new BicicletaComponente(bicicletas.get(1), componente));
+        }
+        for (Componente componente : componentesCanyon) {
+            bicicletaComponentes.add(new BicicletaComponente(bicicletas.get(4), componente));
         }
         bicicletaComponenteRepository.saveAll(bicicletaComponentes);
-
-
-
-
     }
 }
