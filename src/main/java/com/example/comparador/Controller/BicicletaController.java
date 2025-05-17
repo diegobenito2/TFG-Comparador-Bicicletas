cbf025a3642ff3dc64e65451d28b4e81d3202854
Diego <diegobenitomartinez02@gmail.com>
2025-05-17 22:03:18 +0200

    Commit 1.12

--- src/main/java/com/example/comparador/Controller/BicicletaController.java
+++ src/main/java/com/example/comparador/Controller/BicicletaController.java
@@ -87,17 +87,13 @@ public class BicicletaController {
     public String getBicicletaComparador(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes) {
         Optional<Bicicleta> bicicleta = service.findById(id);
         if (bicicleta.isPresent() && !bicicleta.get().getComponentes().isEmpty()) {
-            List<String> ordenDeseado = Arrays.asList(
-                    "Cuadro", "Horquilla", "Amortiguador", "Ruedas", "Cubiertas",
-                    "Frenos", "Disco", "Cambio Trasero", "Desviador", "Cassette",
-                    "Cadena", "Bielas", "Plato", "Pedalier", "Manillar",
-                    "Manetas Cambio", "Tija Sillin", "Sillin", "Bujes"
-            );
+
             List<Bicicleta> listaBicicletas = service.findByTipo(bicicleta.get().getTipo().toString());
-            model.addAttribute("componentesOrdenados", ordenDeseado);
+            model.addAttribute("componentesOrdenados", ordenDeseado(bicicleta.get().getTipo().toString()));
             model.addAttribute("componentes", bicicleta.get().getComponentes());
             model.addAttribute("bicicleta", bicicleta.get());
             if (!listaBicicletas.isEmpty()) {
+                listaBicicletas.remove(bicicleta.get()); // Elimina de la lista la bici por la que has entrado en el comparador.
                 model.addAttribute("bicicletas", listaBicicletas);
             }
             return "comparador";
@@ -107,5 +103,36 @@ public class BicicletaController {
             return "redirect:/";
         }
     }
+    public static List<String> ordenDeseado(String tipoBicicleta) {
+        List<String> ordenDeseado = null;
+        switch (tipoBicicleta) {
+            case "Carretera":
+                ordenDeseado = Arrays.asList(
+                        "Cuadro", "Horquilla", "Ruedas", "Cubiertas",
+                        "Frenos", "Disco", "Cambio Trasero", "Desviador", "Cassette",
+                        "Cadena", "Bielas", "Plato", "Manillar",
+                        "Manetas Cambio", "Tija Sillin", "Sillin", "Bujes", "Pedalier"
+                );
+                break;
+            case "Montaña":
+                ordenDeseado = Arrays.asList(
+                        "Cuadro", "Horquilla", "Amortiguador", "Ruedas", "Cubiertas",
+                        "Frenos", "Disco", "Cambio Trasero", "Cassette",
+                        "Cadena", "Bielas", "Plato", "Manillar",
+                        "Manetas Cambio", "Tija Sillin", "Sillin", "Bujes", "Pedalier"
+                );
+                break;
+            case "Gravel":
+                ordenDeseado = Arrays.asList(
+                        "Cuadro", "Horquilla", "Ruedas", "Cubiertas",
+                        "Frenos", "Disco", "Cambio Trasero", "Cassette",
+                        "Cadena", "Bielas", "Plato", "Manillar",
+                        "Manetas Cambio", "Tija Sillin", "Sillin", "Bujes", "Pedalier"
+                );
+                break;
+        }
+        return ordenDeseado;
+    }
+
 
 }
