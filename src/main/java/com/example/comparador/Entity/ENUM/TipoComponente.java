963f89501f3b2a8b5c822e7126e71c43e2a1f4d2
Diego <diegobenitomartinez02@gmail.com>
2025-05-17 21:09:13 +0200

    Commit 1.11

--- src/main/java/com/example/comparador/Entity/ENUM/TipoComponente.java
+++ src/main/java/com/example/comparador/Entity/ENUM/TipoComponente.java
@@ -1,24 +1,35 @@
 package com.example.comparador.Entity.ENUM;
 
 public enum TipoComponente {
-    Cuadro,
-    Horquilla,
-    Amortiguador,
-    Manillar,
-    Desviador,
-    Manetas_Cambio,
-    Cambio_Trasero,
-    Plato,
-    Bielas,
-    Cassette,
-    Cadena,
-    Frenos,
-    Disco,
-    Ruedas,
-    Cubiertas,
-    Tija_Sillin,
-    Sillin,
-    Bujes,
-    Pedalier
+    Cuadro("Cuadro"),
+    Horquilla("Horquilla"),
+    Amortiguador("Amortiguador"),
+    Manillar("Manillar"),
+    Desviador("Desviador"),
+    Manetas_Cambio("Manetas Cambio"),
+    Cambio_Trasero("Cambio Trasero"),
+    Plato("Plato"),
+    Bielas("Bielas"),
+    Cassette("Cassette"),
+    Cadena("Cadena"),
+    Frenos("Frenos"),
+    Disco("Disco"),
+    Ruedas("Ruedas"),
+    Cubiertas("Cubiertas"),
+    Tija_Sillin("Tija Sillin"),
+    Sillin("Sillin"),
+    Bujes("Bujes"),
+    Pedalier("Pedalier");
+
+    private final String nombre;
+
+    TipoComponente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
+
 
